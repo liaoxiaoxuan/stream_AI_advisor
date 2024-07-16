489cf1ee4546d20b9fcc3941a0df13c0647c95a2
liaoxiaoxuan <0angel.wisdom0@gmail.com>
2024-07-16 19:51:06 +0800

    add comment

--- src/data_processing_netflix.py
+++ src/data_processing_netflix.py
@@ -5,8 +5,14 @@ from utils.data_utils import preprocess_data, save_data
 
 
 # 定義數據文件路徑
+# # 透過 os.path.join() 函式將導入文件路徑，這個方法具有話平台的兼容性
 
+# 原始數據
 raw_data_path = os.path.join('data', 'raw', 'netflix.csv')
+# 整理過後
 processed_data_path = os.path.join('data', 'processed', 'netflix_cleaned.csv')
 
-    # 透過 os.path.join() 函式將導入文件路徑，這個方法具有話平台的兼容性
+
+
+
+
