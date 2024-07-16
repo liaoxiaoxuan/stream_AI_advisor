5835c82aa737016ea94ab1d87750ceb70b88e3d3
liaoxiaoxuan <0angel.wisdom0@gmail.com>
2024-07-16 19:49:37 +0800

    add the path of the data

--- src/data_processing_netflix.py
+++ src/data_processing_netflix.py
@@ -4,3 +4,9 @@ from utils.data_utils import preprocess_data, save_data
 
 
 
+# 定義數據文件路徑
+
+raw_data_path = os.path.join('data', 'raw', 'netflix.csv')
+processed_data_path = os.path.join('data', 'processed', 'netflix_cleaned.csv')
+
+    # 透過 os.path.join() 函式將導入文件路徑，這個方法具有話平台的兼容性
