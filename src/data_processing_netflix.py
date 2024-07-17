a7209561aa9f7860926424ec6b4a53732cb22c45
liaoxiaoxuan <0angel.wisdom0@gmail.com>
2024-07-17 18:01:57 +0800

    add output Preprocessed Netflix data to terminal

--- src/data_processing_netflix.py
+++ src/data_processing_netflix.py
@@ -28,40 +28,45 @@ if __name__ == "__main__":
     print("Loading raw Netflix data...")  # 輸出通知：即將載入原始的 Netflix 資料
     df_netflix = collect_data(raw_data_path)  # 調用 collect_data 函數，從 raw_data_path 指定的路徑載入原始的 Netflix 資料，並將結果存儲在變數 df_netflix 中
 
-    # 輸出載入的數據
-    print("Netflix data loaded successfully:")
-    # 定義要寫入的內容
-    content = (
-        f"Netflix data loaded successfully:\n"
-        f"前幾行：\n{df_netflix.head()}\n"
-        "-----------------------------------------------------\n"
-        f"列數和欄數：\n{df_netflix.shape}\n"
-        "-----------------------------------------------------\n"
-        f"總數據量：\n{df_netflix.size}\n"
-        "-----------------------------------------------------\n"
-        f"所有欄位名稱：\n{df_netflix.columns}\n"
-        "-----------------------------------------------------\n"
-        f"摘要資訊：\n{df_netflix.info()}\n"
-        "-----------------------------------------------------\n"
-        f"記憶體使用量：\n{df_netflix.memory_usage()}\n"
-        "-----------------------------------------------------\n"
-        f"重複行的數量：\n{df_netflix.duplicated().sum()}\n"
-        "-----------------------------------------------------\n"
-        f"每個欄位的缺失值數量：\n{df_netflix.isna().sum()}\n"
-        "-----------------------------------------------------\n"
-        f"所有欄位的數據類型：\n{df_netflix.dtypes.unique()}\n"
-        "-----------------------------------------------------\n"
-    )
+    # # 輸出載入的數據
+    # print("Netflix data loaded successfully:")
+    # # 定義要寫入的內容
+    # content = (
+        # f"Netflix data loaded successfully:\n"
+        # f"前幾行：\n{df_netflix.head()}\n"
+        # "-----------------------------------------------------\n"
+        # f"列數和欄數：\n{df_netflix.shape}\n"
+        # "-----------------------------------------------------\n"
+        # f"總數據量：\n{df_netflix.size}\n"
+        # "-----------------------------------------------------\n"
+        # f"所有欄位名稱：\n{df_netflix.columns}\n"
+        # "-----------------------------------------------------\n"
+        # f"摘要資訊：\n{df_netflix.info()}\n"
+        # "-----------------------------------------------------\n"
+        # f"記憶體使用量：\n{df_netflix.memory_usage()}\n"
+        # "-----------------------------------------------------\n"
+        # f"重複行的數量：\n{df_netflix.duplicated().sum()}\n"
+        # "-----------------------------------------------------\n"
+        # f"每個欄位的缺失值數量：\n{df_netflix.isna().sum()}\n"
+        # "-----------------------------------------------------\n"
+        # f"所有欄位的數據類型：\n{df_netflix.dtypes.unique()}\n"
+        # "-----------------------------------------------------\n"
+    # )
     
-    # 寫入到txt文件中
-    output_file = os.path.join('reports', 'collect_data', 'netflix_data_summary.txt')
-    with open(output_file, "w", encoding="utf-8") as file:
-        file.write(content)
+    # # 寫入到txt文件中
+    # output_file = os.path.join('reports', 'collect_data', 'netflix_data_summary.txt')
+    # with open(output_file, "w", encoding="utf-8") as file:
+        # file.write(content)
     
-    print(f"資料摘要已成功寫入到 {output_file} 文件中。")
+    # print(f"資料摘要已成功寫入到 {output_file} 文件中。")
 
 
 
-    # # 預處理數據
-    # print("Preprocessing Netflix data...")   # 輸出通知：即將預處理原始的 Netflix 資料
-    # df_netflix_cleaned = preprocess_data(df_netflix)  # 調用 preprocess_data 函數，對 df_netflix 中的資料進行預處理，並將結果存儲在變數 df_netflix_cleaned 中
+    # 預處理數據
+    print("Preprocessing Netflix data...")   # 輸出通知：即將預處理原始的 Netflix 資料
+    df_netflix_cleaned = preprocess_data(df_netflix)  # 調用 preprocess_data 函數，對 df_netflix 中的資料進行預處理，並將結果存儲在變數 df_netflix_cleaned 中
+
+    # 輸出整理結果至 terminal
+    print(f"Preprocessing Netflix data（first 20 rows）：\n{df_netflix_cleaned.head(20)}\n")
+    
+    
