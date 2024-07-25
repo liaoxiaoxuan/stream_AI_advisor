5f4b10a4ce47843ffb17ca81aa79e951dfbec5f8
liaoxiaoxuan <0angel.wisdom0@gmail.com>
2024-07-25 15:06:26 +0800

    Added code to plot and save bar chart of Netflix movie release years

--- src/eda_netflix.py
+++ src/eda_netflix.py
@@ -142,104 +142,129 @@ netflix_overall = pd.read_csv(processed_data_path)
 
 
 
-# 分析上架日期（'date_add'列），並產生圖表（影片更新頻率熱力圖）
+# # 分析上架日期（'date_add'列），並產生圖表（影片更新頻率熱力圖）
 
 
-# 確保 'date_added' 列是日期時間格式
-netflix_overall['date_added'] = pd.to_datetime(netflix_overall['date_added'], format='%Y/%m/%d')
-# pd.to_datetime 用來將 date_added 列轉換為 datetime 類型
-# print(netflix_overall['date_added'])
+# # 確保 'date_added' 列是日期時間格式
+# netflix_overall['date_added'] = pd.to_datetime(netflix_overall['date_added'], format='%Y/%m/%d')
+# # pd.to_datetime 用來將 date_added 列轉換為 datetime 類型
+# # print(netflix_overall['date_added'])
 
-# 提取年份和月份
-netflix_date = netflix_overall[['date_added']].dropna()  # 從 netflix_overall 中提取 'date_added' 列，並刪除空值
-netflix_date['year'] = netflix_date['date_added'].dt.year  # 提取年份
-netflix_date['month'] = netflix_date['date_added'].dt.month_name()  # 提取月份名稱
-# 檢查創建的列
-# print(netflix_date.head())
-# print(netflix_date.columns)
-# 計算每個年份中各個月份的頻次
-month_counts = netflix_date.groupby('year')['month'].value_counts()  # month_counts 將會是一個 Series，其中包含每個年份和月份的計數，索引是 MultiIndex，第一層是年份，第二層是月份。
-# print(month_counts)
+# # 提取年份和月份
+# netflix_date = netflix_overall[['date_added']].dropna()  # 從 netflix_overall 中提取 'date_added' 列，並刪除空值
+# netflix_date['year'] = netflix_date['date_added'].dt.year  # 提取年份
+# netflix_date['month'] = netflix_date['date_added'].dt.month_name()  # 提取月份名稱
+# # 檢查創建的列
+# # print(netflix_date.head())
+# # print(netflix_date.columns)
+# # 計算每個年份中各個月份的頻次
+# month_counts = netflix_date.groupby('year')['month'].value_counts()  # month_counts 將會是一個 Series，其中包含每個年份和月份的計數，索引是 MultiIndex，第一層是年份，第二層是月份。
+# # print(month_counts)
 
-# 定義月份順序，並反轉順序
-month_order = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']  #[::-1]
-# print(month_order)
+# # 定義月份順序，並反轉順序
+# month_order = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']  #[::-1]
+# # print(month_order)
 
-# 排列為矩陣
-matrix = month_counts.unstack()  # 先使用 unstack() 將月份從行索引轉為列索引，未出現的月份會變成 NaN
-matrix = matrix.fillna(0)  # 使用 fillna(0) 將 NaN 值填充為 0
-matrix = matrix.astype(int)  # 將數據類型轉換為整數
-matrix = matrix[month_order]  # 根據指定的月份順序重新排序列
-result = matrix.T  # 將矩陣轉置，讓年份成為列索引，月份成為行索引
-# print(result)
-
-
-# # 計算年和月的總和
-
-# result['Month Total'] = result.sum(axis=1)  # 計算每年的總和，並將結果新增至 DataFrame 的新列 'Year Total'
-# result.loc['Year Total'] = result.sum(axis=0)  # 計算每月的總和，並將結果新增至 DataFrame 的新行 'Month Total'
+# # 排列為矩陣
+# matrix = month_counts.unstack()  # 先使用 unstack() 將月份從行索引轉為列索引，未出現的月份會變成 NaN
+# matrix = matrix.fillna(0)  # 使用 fillna(0) 將 NaN 值填充為 0
+# matrix = matrix.astype(int)  # 將數據類型轉換為整數
+# matrix = matrix[month_order]  # 根據指定的月份順序重新排序列
+# result = matrix.T  # 將矩陣轉置，讓年份成為列索引，月份成為行索引
+# # print(result)
 
 
-# # 繪製表格
+# # # 計算年和月的總和
 
-# # 設置圖表大小和分辨率
-# plt.figure(figsize=(12, 8), dpi=300)
-# # plt.figure(): 創建一個新的圖形對象，這是 Matplotlib 用來顯示和處理圖形的容器。
-# # figsize=(12, 8): 設定圖形的尺寸。figsize 是一個元組，表示圖形的寬度和高度，單位是英寸（inches）。
-# # dpi=200: 設定圖形的解析度。dpi 代表 "dots per inch"（每英寸點數），它決定了圖形的解析度。在這個例子中，dpi=200 表示每英寸有 200 個點，這將使圖形的細節更為清晰。高 dpi 值通常用於生成高質量的圖像文件。
+# # result['Month Total'] = result.sum(axis=1)  # 計算每年的總和，並將結果新增至 DataFrame 的新列 'Year Total'
+# # result.loc['Year Total'] = result.sum(axis=0)  # 計算每月的總和，並將結果新增至 DataFrame 的新行 'Month Total'
 
-# table = plt.table(cellText=result.values,  # cellText：表格中的數據
-                #   rowLabels=result.index,  # rowLabels：表格的行標籤
-                #   colLabels=result.columns,  # colLabels：表格的列標籤
-                #   cellLoc='center',  # cellLoc：表格單元格中文字的位置
-                #   loc='center')  # loc：表格在圖中的位置
 
-# # 設置表格樣式
-# table.auto_set_font_size(True)  # 自動調整字體大小
-# table.set_fontsize(10)  # 設置字體大小
-# table.scale(1, 1)  # 調整表格的縮放比例
+# # # 繪製表格
 
-# # 繪製格線（使用 table 的內建方法）
-# for key, cell in table.get_celld().items():  # 遍歷表格中的所有單元格，返回的 key 是單元格的行列索引，cell 是單元格對象
-    # if key[0] in result.index and key[1] in result.columns:  # 檢查單元格的行和列是否在結果的索引和列中
-        # cell.set_edgecolor('black')  # 設置單元格邊框顏色為黑色
-        # cell.set_linewidth(0.05)  # 設置單元格邊框的線寬
+# # # 設置圖表大小和分辨率
+# # plt.figure(figsize=(12, 8), dpi=300)
+# # # plt.figure(): 創建一個新的圖形對象，這是 Matplotlib 用來顯示和處理圖形的容器。
+# # # figsize=(12, 8): 設定圖形的尺寸。figsize 是一個元組，表示圖形的寬度和高度，單位是英寸（inches）。
+# # # dpi=200: 設定圖形的解析度。dpi 代表 "dots per inch"（每英寸點數），它決定了圖形的解析度。在這個例子中，dpi=200 表示每英寸有 200 個點，這將使圖形的細節更為清晰。高 dpi 值通常用於生成高質量的圖像文件。
 
-# # 隱藏坐標軸
-# plt.axis('off')
+# # table = plt.table(cellText=result.values,  # cellText：表格中的數據
+                # #   rowLabels=result.index,  # rowLabels：表格的行標籤
+                # #   colLabels=result.columns,  # colLabels：表格的列標籤
+                # #   cellLoc='center',  # cellLoc：表格單元格中文字的位置
+                # #   loc='center')  # loc：表格在圖中的位置
 
-# # 保存表格為圖片
-# plt_file_count_table = os.path.join('reports', 'collect_data', 'N_date_add_count_table.png')  # 表格的儲存路徑
-# os.makedirs(os.path.dirname(plt_file_count_table), exist_ok=True)  # 創建圖片儲存目錄（如果不存在的話）
-# plt.savefig(plt_file_count_table, bbox_inches='tight')  # 使用 plt.savefig 函數將當前的圖表保存到指定的文件路徑
+# # # 設置表格樣式
+# # table.auto_set_font_size(True)  # 自動調整字體大小
+# # table.set_fontsize(10)  # 設置字體大小
+# # table.scale(1, 1)  # 調整表格的縮放比例
+
+# # # 繪製格線（使用 table 的內建方法）
+# # for key, cell in table.get_celld().items():  # 遍歷表格中的所有單元格，返回的 key 是單元格的行列索引，cell 是單元格對象
+    # # if key[0] in result.index and key[1] in result.columns:  # 檢查單元格的行和列是否在結果的索引和列中
+        # # cell.set_edgecolor('black')  # 設置單元格邊框顏色為黑色
+        # # cell.set_linewidth(0.05)  # 設置單元格邊框的線寬
+
+# # # 隱藏坐標軸
+# # plt.axis('off')
+
+# # # 保存表格為圖片
+# # plt_file_count_table = os.path.join('reports', 'collect_data', 'N_date_add_count_table.png')  # 表格的儲存路徑
+# # os.makedirs(os.path.dirname(plt_file_count_table), exist_ok=True)  # 創建圖片儲存目錄（如果不存在的話）
+# # plt.savefig(plt_file_count_table, bbox_inches='tight')  # 使用 plt.savefig 函數將當前的圖表保存到指定的文件路徑
+# # plt.show()  # 顯示當前圖表，使其在螢幕上顯示出來，這對於交互式環境特別有用
+
+
+# # 繪製熱力圖
+# plt.figure(figsize=(10, 7), dpi=200)  # 設置圖表大小和分辨率
+# plt.pcolor(result, cmap='afmhot_r', edgecolors='white', linewidths=2)  # 使用顏色地圖繪製熱力圖
+# plt.xticks(np.arange(0.5, len(result.columns), 1), result.columns, fontsize=7, fontfamily='serif')  # 設置 x 軸標籤
+# plt.yticks(np.arange(0.5, len(result.index), 1), result.index, fontsize=7, fontfamily='serif')  # 設置 y 軸標籤
+# plt.title('Netflix Contents Update', fontsize=12, fontfamily='calibri', fontweight='bold', position=(0.20, 1.0+0.02))  # 設置圖表標題
+# cbar = plt.colorbar()  # 顯示顏色條
+# cbar.ax.tick_params(labelsize=8)  # 設置顏色條標籤大小
+# cbar.ax.minorticks_on()  # 啟用顏色條的小刻度
+
+# # # 在熱力圖的每個單元格上顯示數值
+# # for i in range(len(result.index)):  # 遍歷行索引
+# #     for j in range(len(result.columns)):  # 遍歷列索引
+# #         plt.text(j + 0.5, i + 0.5, int(result.iloc[i, j]),  # 在 (j + 0.5, i + 0.5) 位置添加文本，文本內容為對應單元格的整數值
+# #                  ha='center', va='center',  # 設置文本的水平和垂直對齊方式為居中
+# #                  fontsize=8, color='black')  # 設置文本的字體大小為 8，顏色為黑色
+
+# # 保存熱力圖為圖片
+# plt_file_count_heatmap = os.path.join('reports', 'figures', 'N_date_add_heatmap.png')  # 表格的儲存路徑
+# os.makedirs(os.path.dirname(plt_file_count_heatmap), exist_ok=True)  # 創建圖片儲存目錄（如果不存在的話）
+# plt.savefig(plt_file_count_heatmap)  # 使用 plt.savefig 函數將當前的圖表保存到指定的文件路徑
 # plt.show()  # 顯示當前圖表，使其在螢幕上顯示出來，這對於交互式環境特別有用
-
-
-# 繪製熱力圖
-plt.figure(figsize=(10, 7), dpi=200)  # 設置圖表大小和分辨率
-plt.pcolor(result, cmap='afmhot_r', edgecolors='white', linewidths=2)  # 使用顏色地圖繪製熱力圖
-plt.xticks(np.arange(0.5, len(result.columns), 1), result.columns, fontsize=7, fontfamily='serif')  # 設置 x 軸標籤
-plt.yticks(np.arange(0.5, len(result.index), 1), result.index, fontsize=7, fontfamily='serif')  # 設置 y 軸標籤
-plt.title('Netflix Contents Update', fontsize=12, fontfamily='calibri', fontweight='bold', position=(0.20, 1.0+0.02))  # 設置圖表標題
-cbar = plt.colorbar()  # 顯示顏色條
-cbar.ax.tick_params(labelsize=8)  # 設置顏色條標籤大小
-cbar.ax.minorticks_on()  # 啟用顏色條的小刻度
-
-# # 在熱力圖的每個單元格上顯示數值
-# for i in range(len(result.index)):  # 遍歷行索引
-#     for j in range(len(result.columns)):  # 遍歷列索引
-#         plt.text(j + 0.5, i + 0.5, int(result.iloc[i, j]),  # 在 (j + 0.5, i + 0.5) 位置添加文本，文本內容為對應單元格的整數值
-#                  ha='center', va='center',  # 設置文本的水平和垂直對齊方式為居中
-#                  fontsize=8, color='black')  # 設置文本的字體大小為 8，顏色為黑色
-
-# 保存熱力圖為圖片
-plt_file_count_heatmap = os.path.join('reports', 'figures', 'N_date_add_heatmap.png')  # 表格的儲存路徑
-os.makedirs(os.path.dirname(plt_file_count_heatmap), exist_ok=True)  # 創建圖片儲存目錄（如果不存在的話）
-plt.savefig(plt_file_count_heatmap)  # 使用 plt.savefig 函數將當前的圖表保存到指定的文件路徑
-plt.show()  # 顯示當前圖表，使其在螢幕上顯示出來，這對於交互式環境特別有用
 
 
 
 # 分析發行年分（'release_year'列），並產生圖表
+
+
+# 繪製長條圖
+plt.figure(figsize=(12,10)) # 設置圖表大小
+sns.set(style="darkgrid") # 設置 Seaborn 的樣式為 "darkgrid"
+ax = sns.countplot(y="release_year", data=netflix_overall, palette="Set2", order=netflix_overall['release_year'].value_counts().index[0:15]) # 使用 Seaborn 繪製柱狀圖，顯示每年發布的電影數量，並按年份排序
+ax.set_title("Number of Movies Released by Netflix Each Year", fontsize=16)  # 設置圖表標題
+
+# 將統計數字顯示在長條圖上
+for container in ax.containers:  # 使用迴圈來依次訪問每根長條
+    ax.bar_label(container, fmt='%d', label_type='edge', padding=3)  # 在當前條形容器上添加數字標籤
+    # fmt='%d': 指定標籤的格式為整數
+    # label_type='edge': 將標籤顯示在長條的邊緣
+    # padding=3: 設定標籤與條形之間的間距為3個像素
+
+
+# 保存圖片
+plot_file = os.path.join('reports', 'collect_data', 'N_release_year_bar.png')  # 使用 os.path.join 函數組合成圖片的儲存路徑
+os.makedirs(os.path.dirname(plot_file), exist_ok=True)  # 使用 os.makedirs 創建圖片儲存目錄（如果不存在的話），exist_ok=True 表示如果目錄已經存在則不報錯
+plt.savefig(plot_file)  # 使用 plt.savefig 函數將當前的圖表保存到指定的文件路徑
+
+plt.show()  # 顯示當前圖表，使其在螢幕上顯示出來，這對於交互式環境特別有用
+
+
+
 # 分析年齡分級（'rating'列），並產生圖表
 # 分析影片時長（'duration'列），並產生圖表
