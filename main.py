2006151da9d8149daa12eeb00c287676b085b398
liaoxiaoxuan <0angel.wisdom0@gmail.com>
2024-09-23 22:15:26 +0800

    Add the file 'main.py'

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
+import argparse
+
+from src.test import eda_netflix_mysql_test
+from src.utils import eda_netflix_mysql_test
+
+def main():
+    eda_netflix_mysql_test.main()
+
 if __name__ == '__main__':
-    pass
+    main()
