ba40e911cb4c29ff1a4f84ea07fc7da4053df814
liaoxiaoxuan <0angel.wisdom0@gmail.com>
2024-09-23 22:33:29 +0800

    Add the file 'eda_sql_netflix.py' and import modules.

---
+++ src/eda_sql_netflix.py
@@ -0,0 +1,13 @@
+import mysql.connector
+import pandas as pd
+import os
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+from dotenv import load_dotenv
+from utils.data_utils_mysql import MySQLConnector
+from utils.data_utils_mysql import Analysis
+
+
