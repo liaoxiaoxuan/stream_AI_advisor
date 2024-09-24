a7c9c5566513dbe4e9045f28b25960911a5a8b6b
liaoxiaoxuan <0angel.wisdom0@gmail.com>
2024-09-24 23:39:03 +0800

    Generated and exported heatmap for content additions by month and year.

--- src/eda_sql_netflix.py
+++ src/eda_sql_netflix.py
@@ -14,7 +14,7 @@ from utils.data_utils_mysql import Analysis
 load_dotenv()
 
 def main():
-    print('test')
+    # print('test')
     # 創建 Analysis 實例，指定資料表名稱
     analysis = Analysis('data_netflix')
     # 從數據庫獲取數據
@@ -22,15 +22,29 @@ def main():
     # 進行數據計算
     analysis.calculate()
 
-    # 分析影片類型（'type'列），並產生圖表
-    # 生成柱狀圖
-    analysis.visualize('bar', 'type')
-    analysis.export('content_type_bar.png')
-    # 生成圓餅圖
-    analysis.visualize('pie', 'type')
-    analysis.export('content_type_pie.png')
 
+    # # 分析影片類型（'type'列），並產生圖表
+
+    # # 生成柱狀圖
+    # analysis.visualize('bar', 'type')
+    # analysis.export('content_type_bar.png')
+
+    # # 生成圓餅圖
+    # analysis.visualize('pie', 'type')
+    # analysis.export('content_type_pie.png')
+
+
+    # 分析上架日期（'date_add'列），並產生圖表
     
+    # # 生成統計表格
+    # analysis.visualize('table', 'date_added')
+    # analysis.export('yearly_content_addition.png')
+
+    # 生成熱力圖
+    analysis.visualize('heatmap', 'month_year')
+    analysis.export('content_addition_heatmap.png')    
+    
+
 
 if __name__ == "__main__":
     main()
