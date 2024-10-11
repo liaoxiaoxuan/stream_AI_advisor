3adf0555281a2fb376e3c342c209d2ee8cdc6a18
liaoxiaoxuan <0angel.wisdom0@gmail.com>
2024-10-11 15:09:39 +0800

    Added Streamlit setup for Netflix and Disney+ streaming recommendation system with title and description.

---
+++ src/streamlit_app.py
@@ -0,0 +1,12 @@
+import time
+
+import streamlit as st
+
+import numpy as np
+import pandas as pd
+
+
+
+# 設置頁面標題與文字
+st.title('Netflix & Disney+ 串流影音平台推薦系統')
+st.write('幫助您在 Netflix 與 Disney+ 之間，作出最適切的選擇！')
