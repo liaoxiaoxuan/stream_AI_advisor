f180dd10ebae0767df74c282219fa4551d021175
liaoxiaoxuan <0angel.wisdom0@gmail.com>
2024-10-12 15:00:44 +0800

    Added the emoji into the file name.

--- src/streamlit_run/Home.py
+++ src/streamlit_run/Home.py
@@ -10,3 +10,9 @@ import pandas as pd
 # 設置頁面標題與文字
 st.title('串流影音平台推薦系統')
 st.write('幫助您在 Netflix 與 Disney+ 之間，作出最適切的選擇！')
+
+
+
+# from chart_view import chart_view_page
+# from search_movies import search_movies_page
+# from match_rate import match_rate_page
