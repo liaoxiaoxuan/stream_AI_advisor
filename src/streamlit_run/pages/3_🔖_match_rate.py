3dd93daeb917197d19487c1abac9a7e0648ac0b3
liaoxiaoxuan <0angel.wisdom0@gmail.com>
2024-10-25 16:13:38 +0800

    Add MySQL connection and data retrieval functions.

--- src/streamlit_run/pages/3_🔖_match_rate.py
+++ src/streamlit_run/pages/3_🔖_match_rate.py
@@ -4,6 +4,10 @@ import seaborn as sns
 import numpy as np
 from matplotlib.colors import LinearSegmentedColormap
 
+import pandas as pd
+import mysql.connector
+from mysql.connector import Error  # 引入MySQL連接器中的Error類，用於處理錯誤
+
 from dotenv import load_dotenv
 import os
 
@@ -90,6 +94,28 @@ def calculate_match_percentage(original_df, filtered_df, platform):
     original_count = len(original_df[original_df['source'] == platform])  # 計算原始數據中該平台的數量
     filtered_count = len(filtered_df[filtered_df['source'] == platform])  # 計算過濾後數據中該平台的數量
     return (filtered_count / original_count * 100) if original_count > 0 else 0  # 計算百分比，避免除以0
+
+
+
+# 連接到MySQL數據庫
+def create_connection(db_config):
+    connection = None
+    try:
+        # 使用提供的配置參數連接到MySQL數據庫
+        connection = mysql.connector.connect(**db_config)
+        print(f"Successfully connected to MySQL database: {db_config['database']}")
+    except Error as e:
+        # 如果發生錯誤，輸出錯誤訊息
+        print(f"The error '{e}' occurred")
+    return connection  # 返回連接物件
+
+
+
+# 從數據庫獲取數據
+def get_data(connection, table):
+    # 從指定的數據表中選取相關的數據字段
+    query = f"SELECT type, title, director, cast, country, release_year, rating, duration, listed_in, description, keywords FROM {table}"
+    return pd.read_sql(query, connection)  # 使用Pandas的read_sql方法執行查詢，並返回數據框
 
 
 
